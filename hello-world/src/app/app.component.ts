208d541170a892404d1d0a9418e8965931b99fc7
XiaoLi <leonpa1987@gmail.com>
2024-08-13 20:52:59 +0800

    add my first component

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CoursesComponent} from "./courses.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CoursesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

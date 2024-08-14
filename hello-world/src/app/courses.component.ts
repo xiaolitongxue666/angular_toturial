1ee11712bde273d3d95d8beee418bc8f512e58d6
xiaolitongxue666 <leonpa1987@gamil.com>
2024-08-14 14:26:12 +0800

    Imterpolation & Dorectives

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,5 +1,6 @@
 // Import decorator
 import { Component } from "@angular/core";
+import {NgForOf} from "@angular/common";
 
 // Apply decorator to the class
 // Call decorator like a function (decorator function)
@@ -7,9 +8,26 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'courses',
   standalone: true,
-  template: '<h2>Courses</h2>'
+  imports: [
+    NgForOf
+  ],
+  template: `
+    <h2>{{ "Title:" + title + " | " + getTitle() }}</h2>
+    <ul>
+      <li *ngFor="let course of courses">
+        {{ course }}
+      </li>
+    </ul>
+  `
 
 })
 export class CoursesComponent2 {
+  title = "List of courses";
+
+  getTitle() {
+    return this.title;
+  }
+
+  courses = ["course1", "course2", "course3"]
 
 }

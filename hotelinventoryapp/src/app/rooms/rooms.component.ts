09b9937cc97410c2d3f6710808b161e0ac5d979d
xiaolitongxue666 <leonpa1987@gamil.com>
2024-08-23 11:28:13 +0800

    Property Bindling example with ngSrc

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'hinv-rooms',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.scss'
 })
 export class RoomsComponent {
-
+  hotelName = 'Hilton Hotel';
+  imageUrl = 'https://p1.itc.cn/q_70/images01/20211122/76b8d5be7e06432882a65b0059200b18.png';
+  isLoading = false;
 }

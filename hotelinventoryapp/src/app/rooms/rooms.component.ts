783106a6b983f5316aff7daf2b21501ac4074e20
xiaolitongxue666 <leonpa1987@gamil.com>
2024-08-23 14:18:08 +0800

    Add event binding

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -14,4 +14,9 @@ export class RoomsComponent {
   hotelName = 'Hilton Hotel';
   imageUrl = 'https://p1.itc.cn/q_70/images01/20211122/76b8d5be7e06432882a65b0059200b18.png';
   isLoading = false;
+  numberOfRooms = 10;
+
+  toggle() {
+    this.numberOfRooms = this.numberOfRooms === 10? 5 : 10;
+  }
 }

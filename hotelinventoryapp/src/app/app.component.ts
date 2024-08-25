cb5cfb4a6647698ab3f253bb8f7bb3a2ee3eb7e2
XiaoLi <leonpa1987@gmail.com>
2024-08-25 21:18:09 +0800

    update

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {RoomsComponent} from "./rooms/rooms.component";
+import {NgSwitch, NgSwitchCase} from "@angular/common";
 
 @Component({
   selector: 'hinv-root',
   standalone: true,
-  imports: [RouterOutlet, RoomsComponent],
+  imports: [RouterOutlet, RoomsComponent, NgSwitchCase, NgSwitch],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
+
+  loginTypes = "Admin";
 }

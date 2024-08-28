ef9f31c75cb61cead68cf776067111bd7cfce729
XiaoLi <leonpa1987@gmail.com>
2024-08-28 18:15:45 +0800

    add component communication

--- hotelinventoryapp/src/app/rooms/rooms-list/rooms-list.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {DatePipe, NgForOf, NgOptimizedImage} from '@angular/common';
 import {RoomList} from "./rooms-list";
 
@@ -15,31 +15,7 @@ import {RoomList} from "./rooms-list";
 })
 export class RoomsListComponent {
 
-  roomList: RoomList[] = [
-    {
-      rootType: 'Deluxe Room',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 500,
-      photos: 'https://pix10.agoda.net/hotelImages/2296893/29598206/97da276e6eec9d266fa6da5d08192cb9.jpg',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-    },
-    {
-      rootType: 'Deluxe Room',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV',
-      price: 300,
-      photos: 'https://pix10.agoda.net/hotelImages/2296893/29598206/97da276e6eec9d266fa6da5d08192cb9.jpg',
-      checkinTime: new Date('12-Nov-2021'),
-      checkoutTime: new Date('13-Nov-2021'),
-    },
-    {
-      rootType: 'Private Suite',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV',
-      price: 1000,
-      photos: 'https://pix10.agoda.net/hotelImages/2296893/29598206/97da276e6eec9d266fa6da5d08192cb9.jpg',
-      checkinTime: new Date('14-Nov-2021'),
-      checkoutTime: new Date('15-Nov-2021'),
-    }
-  ]
+  @Input() rooms: RoomList[] = [];
 
 }
+
--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {NgIf, NgOptimizedImage} from '@angular/common';
 import {Room} from './rooms';
 import {RoomsListComponent} from "./rooms-list/rooms-list.component";
+import {RoomList} from "./rooms-list/rooms-list";
 
 @Component({
   selector: 'hinv-rooms',
@@ -29,4 +30,31 @@ export class RoomsComponent {
   toggle() {
     this.numberOfRooms = this.numberOfRooms === 10 ? 5 : 10;
   }
+
+  roomList: RoomList[] = [
+    {
+      rootType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos: 'https://pix10.agoda.net/hotelImages/2296893/29598206/97da276e6eec9d266fa6da5d08192cb9.jpg',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
+    },
+    {
+      rootType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV',
+      price: 300,
+      photos: 'https://pix10.agoda.net/hotelImages/2296893/29598206/97da276e6eec9d266fa6da5d08192cb9.jpg',
+      checkinTime: new Date('12-Nov-2021'),
+      checkoutTime: new Date('13-Nov-2021'),
+    },
+    {
+      rootType: 'Private Suite',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV',
+      price: 1000,
+      photos: 'https://pix10.agoda.net/hotelImages/2296893/29598206/97da276e6eec9d266fa6da5d08192cb9.jpg',
+      checkinTime: new Date('14-Nov-2021'),
+      checkoutTime: new Date('15-Nov-2021'),
+    }
+  ]
 }

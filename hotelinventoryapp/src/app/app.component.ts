6c86763fe41003bba0d277ed7be93bf26321e287
XiaoLi <leonpa1987@gmail.com>
2024-09-24 20:18:43 +0800

    reset html part

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NgSwitch, NgSwitchCase } from "@angular/common";
-import { ContainerComponent } from "./container/container.component";
-import { EmployeeComponent } from "./employee/employee.component";
-import { APP_CONFIG_PROVIDER } from "./AppConfig/appconfig.service";
-import { InitService } from "./init.service";
+import {AfterViewInit, Component, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {Subscription} from 'rxjs';
+import {timeout} from 'rxjs/operators';
+import {RoomsComponent} from "./rooms/rooms.component";
+import {NgSwitch, NgSwitchCase} from "@angular/common";
+import {ContainerComponent} from "./container/container.component";
+import {EmployeeComponent} from "./employee/employee.component";
+import {APP_CONFIG_PROVIDER} from "./AppConfig/appconfig.service"
+import {Observable} from "rxjs";
+import { InitService } from "./init.service"
 
 @Component({
   selector: 'hinv-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    RoomsComponent,
     NgSwitchCase,
     NgSwitch,
     ContainerComponent,
@@ -20,14 +26,65 @@ import { InitService } from "./init.service";
   styleUrl: './app.component.scss',
   providers: [APP_CONFIG_PROVIDER]  // Removed InitService from here
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements AfterViewInit, OnInit {
   title = 'hotelinventoryapp';
   loginTypes = "Admin";
 
-  constructor(private init_service: InitService) {}
+  data: any; // 用于存储获取到的 JSON 数据
+  subscription: Subscription; // 用于存储对 HTTP 请求的订阅
+
+  constructor(private http: HttpClient, private init_service: InitService) {
+    this.subscription = Subscription.EMPTY; // 初始化 subscription
+  }
+
+  @ViewChild('user', {read: ViewContainerRef}) vcr!: ViewContainerRef;
+
+  ngAfterViewInit() {
+    const componentRef = this.vcr.createComponent(RoomsComponent);
+    componentRef.instance.numberOfRooms = 50;
+  }
 
   ngOnInit(): void {
-    // Log the config directly
+
+    // Show init service config
     console.log('Init service config:', this.init_service.config);
+
+    // 在 ngOnInit 中订阅 fetchData()
+    //   this.subscription = this.fetchData().subscribe({
+    //     next: (data) => {
+    //       // 处理成功获取到的数据
+    //       this.data = data;
+    //       console.log('数据已获取：', this.data); // 打印获取到的 JSON 数据
+    //     },
+    //     error: (error) => {
+    //       // 处理请求错误
+    //       console.error('请求错误：', error);
+    //       // Provide feedback to the user
+    //     },
+    //     complete: () => {
+    //       // 处理请求完成
+    //       console.log('请求完成');
+    //     }
+    //   });
+
   }
+
+  // 获取数据的方法
+  // fetchData(): Observable<any> {
+  //   // 使用 HttpClient 发送 GET 请求到指定的 URL
+  //
+  //   // return this.http.get('https://jsonplaceholder.typicode.com/todos/1').pipe(timeout(8000));
+  //
+  //   // return this.http.get('http://localhost:3000/api/Rooms/').pipe(timeout(8000));
+  //
+  //   return this.http.get('/api/Rooms/').pipe(timeout(8000));
+  // }
+
+  ngOnDestroy(): void {
+    // 在组件销毁之前取消订阅
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
 }

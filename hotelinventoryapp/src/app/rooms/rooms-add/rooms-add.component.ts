7e50a30e63ca0de09dc4a2ab84217eabd8fd6010
xiaolitongxue666 <leonpa1987@gamil.com>
2024-09-29 15:49:34 +0800

    add component rooms-add

---
+++ hotelinventoryapp/src/app/rooms/rooms-add/rooms-add.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hinv-rooms-add',
+  standalone: true,
+  imports: [],
+  templateUrl: './rooms-add.component.html',
+  styleUrl: './rooms-add.component.scss'
+})
+export class RoomsAddComponent {
+
+}

a0a4d394ab1baf355fb17287e91bf22f51c3aadc
xiaolitongxue666 <leonpa1987@gamil.com>
2024-10-08 16:23:58 +0800

    add route navigate

--- hotelinventoryapp/src/app/login/login.component.ts
+++ hotelinventoryapp/src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import {FormsModule} from "@angular/forms";
 import {HoverDirective} from "../hover.directive";
 import {EmailvalidatorDirective} from "../emailvalidator/emailvalidator.directive";
 import {JsonPipe} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'hinv-login',
@@ -21,7 +22,7 @@ export class LoginComponent implements OnInit {
   email: string='';
   password: string='';
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
 
@@ -30,6 +31,9 @@ export class LoginComponent implements OnInit {
   login() {
     if(this.email === 'admin@example.com' && this.password === 'password') {
       alert("Login Successful");
+      this.route.navigate(['/rooms']);
+      // this.route.navigate(['/rooms', 'add']);
+      // this.route.navigateByUrl('/rooms/add');
     }
   }
 }

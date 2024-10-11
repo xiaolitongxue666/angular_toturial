c693bd8c1c7b5245ab6bf5e79adafb7e1cf06ade
XiaoLi <leonpa1987@gmail.com>
2024-10-11 21:21:10 +0800

    add reactive form

--- hotelinventoryapp/src/app/rooms/rooms-booking/rooms-booking.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms-booking/rooms-booking.component.ts
@@ -3,14 +3,19 @@ import {ActivatedRoute} from "@angular/router";
 import {catchError, map, Observable, of, Subject} from "rxjs";
 import {RoomsService} from "../services/rooms.service";
 import {RoomList} from "../rooms-list/rooms-list";
-import {JsonPipe} from "@angular/common";
+import {JsonPipe, NgIf} from "@angular/common";
+// Reactive Forms
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'hinv-rooms-booking',
   standalone: true,
   templateUrl: './rooms-booking.component.html',
   imports: [
-    JsonPipe
+    JsonPipe,
+    ReactiveFormsModule,
+    NgIf
   ],
   styleUrls: ['./rooms-booking.component.scss']
 })
@@ -21,7 +26,26 @@ export class RoomsBookingComponent implements OnInit {
   error$ = new Subject<string>();
   modified_room: RoomList | undefined;
 
-  constructor(private router: ActivatedRoute, private roomsService: RoomsService) { }
+  // Reactive Form
+  roomBookingForm : FormGroup;
+
+  constructor(private router: ActivatedRoute,
+              private roomsService: RoomsService) {
+
+    // Reactive Form
+    this.roomBookingForm = new FormGroup({
+      roomNumber: new FormControl('', [Validators.required]),
+      roomType: new FormControl('', [Validators.required]),
+      amenities: new FormControl('', [Validators.required]),
+      price: new FormControl('', [Validators.required]),
+      photos: new FormControl('', [Validators.required]),
+      checkinTime: new FormControl('', [Validators.required]),
+      checkoutTime: new FormControl('', [Validators.required]),
+      rating: new FormControl('', [Validators.required])
+
+    });
+
+  }
 
   ngOnInit() {
     // Get the roomId from the URL
@@ -36,6 +60,8 @@ export class RoomsBookingComponent implements OnInit {
         this.modified_room = this.roomList.find(room => room.roomNumber === this.roomId.toString());
         if (this.modified_room) {
           console.log('Found Room:', this.modified_room);
+          // 使用 patchValue 设置表单值
+          this.roomBookingForm.patchValue(this.modified_room);
         } else {
           console.log('No room found with the specified room number.');
         }
@@ -46,4 +72,20 @@ export class RoomsBookingComponent implements OnInit {
       })
     ).subscribe();
   }
+
+  onSubmit() {
+    if (this.roomBookingForm.valid) {
+      const roomData: RoomList = {
+        ...this.roomBookingForm.value,
+        checkinTime: new Date(this.roomBookingForm.value.checkinTime),
+        checkoutTime: new Date(this.roomBookingForm.value.checkoutTime),
+      };
+      console.log('提交的房间数据:', roomData);
+      //  在此处处理提交的数据，例如发送到后端
+      this.roomsService.editRoom(roomData).subscribe((data) => {
+        this.roomList = data;
+      })
+    }
+  }
+
 }
